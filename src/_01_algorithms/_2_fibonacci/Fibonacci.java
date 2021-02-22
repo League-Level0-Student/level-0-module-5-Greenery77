dd1f462ef0304bbd71733d64eb595990d58512a4
root <root@53527ae3a108>
2021-02-22 21:22:49 +0000

    Start: Amazing Rings

--- src/_01_algorithms/_2_fibonacci/Fibonacci.java
+++ src/_01_algorithms/_2_fibonacci/Fibonacci.java
@@ -7,16 +7,20 @@ public static void main(String[] args) {
 	
 int number1 = 0;
 int number2 = 1;
-int sum = number1 + number2;
-	
-System.out.println(number1 + " " + number2);
+
+// 0 1 1 2 3 5 8 13 21  
+
+System.out.print(number1 + " " + number2 + " ");
 		
 
 for (int i = 0; i < 10; i++) {
 	
-System.out.println(sum);
+int sum = number1 + number2;
 
+System.out.print(sum + " ");
 
+number1 = number2;
+number2 = sum;
 
 }
 	
