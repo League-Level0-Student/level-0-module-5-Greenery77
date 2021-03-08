02ef00ba7034ee79e2f8ab7dd320115ee6e51e27
root <root@a075445fbb22>
2021-03-08 21:29:16 +0000

    For Gauntlet Work In Progress

--- src/_02_nested_loops/_3_for_loop_gauntlet/ForGauntlet.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/ForGauntlet.java
@@ -12,14 +12,124 @@ public class ForGauntlet {
 //	}
 	
 	
-	for (int i = 100; i >= 0; i--) {
+//	for (int i = 100; i >= 0; i--) {
+//		
+//		System.out.println(i);
+//		
+//	}	
 		
-		System.out.println(i);
 		
-	}	
+//	for (int i = 0; i < 100; i++) {
+//		
+//		if (i % 2 == 0) {
+//			
+//			System.out.println(i);
+//			
+//		}
+//		
+//	}	
 		
 		
+//	for (int i = 0; i < 99; i++) {
+//		
+//		if (i % 2 == 1) {
+//			
+//			System.out.println(i);
+//			
+//		}
+//		
+//	}	
 		
+		
+//	for (int i = 1; i < 501; i++) {
+//		
+//		if (i % 2 == 0) {
+//			
+//			System.out.println(i + " is even.");
+//			
+//		}
+//		
+//		else {
+//			
+//			System.out.println(i + " is odd.");
+//			
+//		}
+//		
+//	}
+		
+		
+//	for (int i = 7; i < 778; i++) {
+//		
+//		if (i % 7 == 0) {
+//			
+//			System.out.println(i);
+//			
+//		}
+//		
+//	}
+		
+		
+//	int j = 0;
+//		
+//	for (int i = 2009; i < 2021; i++) {
+//		
+//		System.out.println("In " + i + ", I was " + j + " years old.");
+//		
+//		j += 1;
+//		
+//	}
+//		
+		
+		
+//	for (int i = 0; i < 3; i++) {
+//		
+//		for (int j = 0; j < 3; j++) {
+//			
+//		System.out.println(i + " " + j);	
+//			
+//		}
+//		
+//	}
+		
+		
+//		int k = 0; 
+//	for (int i = 1; i < 4; i++) {
+//		
+//		for (int j = 1; j < 4; j++) {
+//
+//			System.out.print((j + k) + " ");
+//			
+//		}
+//		k+=3;
+//		System.out.println();
+//		
+//	}	
+		
+		
+//	for (int i = 0; i < 7; i+=3) {
+//		
+//		for (int j = 1; j < 4; j++) {
+//
+//			System.out.print((j + i) + " ");
+//			
+//		}
+//
+//		System.out.println();
+//		
+//	}	
+	
+	
+//for (int i = 0; i < 91; i+=10) {
+//		
+//		for (int j = 1; j < 11; j++) {
+//
+//			System.out.print((j + i) + " ");
+//			
+//		}
+//
+//		System.out.println();
+//		
+//}
 		
 		
 		
@@ -29,10 +139,6 @@ public class ForGauntlet {
 		
 		
 	}
-	
-	
-	
-	
 	
 	
 }
